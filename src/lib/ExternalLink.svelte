bd35e9ee676e9bbe3e6d43e4ea3f1732fba36ba9
jachang820 <j.a.chang820@gmail.com>
2024-07-01 05:04:33 -0700

    added external link components

---
+++ src/lib/ExternalLink.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import Icon from "$lib/Icon.svelte";
+    import { FaExternalLinkSquareAlt } from "$lib/icons";
+
+    let className = "";
+
+    // Props
+    export let href:string;
+    export let iconSize:number = 16;
+    export {className as class};
+
+    /** CSS variable props
+     * --external-link-icon-color: black
+     */
+
+</script>
+
+<a {href} target="_blank" rel="noopener noreferrer" class={className}>
+    <slot /><Icon icon={FaExternalLinkSquareAlt}
+                  width={iconSize} height={iconSize}
+                  class="external-link-icon" />
+</a>
+
+<style>
+
+    a {
+        color: var(--external-link-color, rgb(0, 105, 194));
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    :global(.external-link-icon) {
+        display: inline-block;
+        color: var(--external-link-icon-color, var(--external-link-color, black));
+        margin-right: 0.25rem;
+        margin-bottom: 1rem;
+    }
+</style>
--- src/lib/MultiLink.svelte
+++ src/lib/MultiLink.svelte
@@ -7,6 +7,10 @@
     export let target: Modal | string = "#";
     export {className as class};
 
+    let externalHref: boolean = target.startsWith("http");
+    let browsingContext: string = externalHref ? "_blank" : "_self";
+    let rel: string = externalHref ? "noreferrer noopener" : "strict-origin";
+
     const openModal = () => {
         (target as Modal).open();
     };
@@ -19,7 +23,9 @@
     </button>
 
 {:else}
-    <a href={target} role="button" tabindex="0" class={className}>
+    <a href={target} role="button" tabindex="0"
+       target={browsingContext} {rel}
+       class={className}>
         <slot />
     </a>
 
